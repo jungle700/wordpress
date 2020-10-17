5239bebfdd3dc658e4f57a6c033ddc700f80895a
jungle700-github <akeem.bakare2000@yahoo.co.uk>
2020-10-17 21:37:30 +0100

    final working version

--- vpcpeer.tf
+++ vpcpeer.tf
@@ -10,9 +10,11 @@ resource "aws_vpc_peering_connection" "foo" {
 }
 
 #....................................route VPC_A to VPC_B.....................................
-resource "aws_route" "primary2secondary" {
+#................................first_subnet connecting to Prometheus subnet...........
+resource "aws_route" "primary2SubnetA" {
   # ID of VPC 1 main route table.
-  route_table_id = aws_route_table.eu-west-1a-prometh.id
+  #route_table_id = aws_route_table.eu-west-1a-prometh.id
+  route_table_id = aws_route_table.eu-west-1a-private.id
 
   # CIDR block / IP range for VPC 2.
   destination_cidr_block = "10.1.0.0/20"
@@ -21,8 +23,23 @@ resource "aws_route" "primary2secondary" {
   vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
 }
 
+#................................second_subnet connecting to Prometheus subnet...........
+resource "aws_route" "primary2SubnetB" {
+  # ID of VPC 1 main route table.
+  #route_table_id = aws_route_table.eu-west-1a-prometh.id
+  
+
+  route_table_id = aws_route_table.eu-west-1b-private.id
+
+  # CIDR block / IP range for VPC 2.
+  destination_cidr_block = "10.1.0.0/20"
+
+  # ID of VPC peering connection.
+  vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
+}
 
 #.........................................route VPC_B to VPC_A..........................
+#good one
 resource "aws_route" "secondary2primary" {
   # ID of VPC 2 main route table.
   route_table_id = aws_route_table.eu-west-1a-prometh.id
